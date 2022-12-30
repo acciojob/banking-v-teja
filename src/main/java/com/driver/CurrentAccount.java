fffbe6ab35a2f6ceb22263f0d743814241cd7ab6
v-teja <tejaviswa95@gmail.com>
2022-12-30 17:37:33 +0530

    getters and setters added

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -3,13 +3,21 @@ package com.driver;
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
+    }
+
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
         super(name,balance,5000);
         if(balance<5000){
             throw new Exception("Insufficient Balance");
         }
-        this.tradeLicenseId = tradeLicenseId;
+        setTradeLicenseId(tradeLicenseId);
     }
 
     public char getMaxCountChar(int[] count){
@@ -30,14 +38,14 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
-        int n = tradeLicenseId.length();
+        int n = getTradeLicenseId().length();
         if(n==0){
             throw new Exception("valid License can not be generated");
         }
         boolean flag = true;
         for(int i=0;i<n-1;i++){
-            char temp1 = tradeLicenseId.charAt(i);
-            char temp2 = tradeLicenseId.charAt(i+1);
+            char temp1 = getTradeLicenseId().charAt(i);
+            char temp2 = getTradeLicenseId().charAt(i+1);
             if(temp1==temp2){
                 flag = false;
                 break;
@@ -49,7 +57,7 @@ public class CurrentAccount extends BankAccount{
             for(int i=0;i<26;i++){
                 count[i] = 0;
             }
-            for(char ch: tradeLicenseId.toCharArray()){
+            for(char ch: getTradeLicenseId().toCharArray()){
                 count[(int)ch-(int)'a']++;
             }
 
@@ -86,8 +94,7 @@ public class CurrentAccount extends BankAccount{
                     count[i]--;
                 }
             }
-            this.tradeLicenseId = res;
-
+            setTradeLicenseId(res);
         }
 
 
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -7,16 +7,32 @@ public class SavingsAccount extends BankAccount{
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
         super(name,balance,0);
-        this.maxWithdrawalLimit= maxWithdrawalLimit;
-        this.rate = rate;
-
+        setMaxWithdrawalLimit(maxWithdrawalLimit);
+        setRate(rate);
     }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
+    public void setMaxWithdrawalLimit(double maxWithdrawalLimit) {
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+    }
+
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
-        if(amount>this.maxWithdrawalLimit){
+        if(amount>getMaxWithdrawalLimit()){
             throw new Exception("Maximum Withdraw Limit Exceed");
         }
 
@@ -31,13 +47,13 @@ public class SavingsAccount extends BankAccount{
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-        double simpleInterest = (getBalance()*rate*years)/100;
+        double simpleInterest = (getBalance()*getRate()*years)/100;
         return simpleInterest+getBalance();
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-        double amount  = getBalance() * (Math.pow((1 + rate / 100*times), times*years));
+        double amount  = getBalance() * (Math.pow((1 + getRate() / 100*times), times*years));
         return amount;
 
     }
