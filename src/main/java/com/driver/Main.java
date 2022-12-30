ae69d2edb8dbca253f0623c696f8aa6e7048811e
v-teja <tejaviswa95@gmail.com>
2022-12-30 20:26:16 +0530

    reverting back

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,5 +3,12 @@ package com.driver;
 public class Main {
     public static void main(String[] args) throws Exception {
 
+<<<<<<< HEAD
+=======
+        BankAccount account = new BankAccount("Viswa",123.4,100);
+        String str = account.generateAccountNumber(6,52);
+        System.out.println(str);
+
+>>>>>>> parent of 4c216d8 (current account changed)
     }
 }
